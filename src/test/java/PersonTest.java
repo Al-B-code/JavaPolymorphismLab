1ad078002518dc860109801e3e68b3f3916b9f8b
Albert Hughes <albieh@me.com>
2023-10-16 17:05:41 +0100

    First commit, empty classes created

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,17 @@
+public abstract class PersonTest {
+
+    protected String name;
+    protected int age;
+    protected String address;
+
+
+    public void Person(String name, int age, String address){
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+
+
+
+}
