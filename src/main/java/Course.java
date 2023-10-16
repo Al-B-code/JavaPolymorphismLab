0ebd14634cf6eabcace364d3d09cc6dc3bd1765b
Albert Hughes <albieh@me.com>
2023-10-16 20:33:57 +0100

    added course class and appropriate tests.

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class Course {
+
+    String courseCode;
+    String courseName;
+    ArrayList<Person> registeredPeople;
+
+
+    public Course(String courseCode, String courseName){
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+
+        registeredPeople = new ArrayList<Person>();
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public ArrayList<Person> getRegisteredPeople() {
+        return registeredPeople;
+    }
+
+    public void addRegisteredPeople(Person person) {
+        registeredPeople.add(person);
+    }
+
 }
--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,2 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class CourseTest {
+
+    Teacher teacher;
+    Student student;
+    Course course;
+
+    @BeforeEach
+    void setUp() {
+        course = new Course("Bio101", "Biology");
+
+        teacher = new Teacher("Brain Cox",55, "55 Broadway, READING, RG35 2CI", 12123434);
+        student = new Student("John Smith", 18, "10 Downing Street", 123456789);
+    }
+
+
+
+    @Test
+    void canGetCourseCode(){
+        assertThat(course.getCourseCode()).isEqualTo("Bio101");
+    }
+
+    @Test
+    void canGetCourseName(){
+        assertThat(course.getCourseName()).isEqualTo("Biology");
+    }
+
+    @Test
+    void canSetCourseName(){
+
+    }
+
+    @Test
+    void canAddRegisteredPerson(){
+        course.addRegisteredPeople(teacher);
+        assertThat(true).isEqualTo(course.registeredPeople.contains(teacher));
+        course.addRegisteredPeople(student);
+        assertThat(true).isEqualTo(course.registeredPeople.contains(student));
+    }
+
+    @Test
+    void canGetRegisteredPeople(){
+        course.addRegisteredPeople(teacher);
+        System.out.println(course.getRegisteredPeople());
+        assertThat(course.getRegisteredPeople().size()).isEqualTo(1);
+    }
 }
