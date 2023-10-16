d3df491b7fd7f9c0b82cfa0195fea60174c6c744
Albert Hughes <albieh@me.com>
2023-10-16 20:42:57 +0100

    Initial creation of the university class.

--- src/main/java/University.java
+++ src/main/java/University.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class University {
 
-
+    private ArrayList<Student> students;
+    private ArrayList<Teacher> teachers;
+    private ArrayList<Course> courses;
+    private HashMap<Teacher, ArrayList<Course>> teacherCourseMap;
 
 
 }
