3a6754143f38bfbdeebd049d440a60b1cdc96fbb
Albert Hughes <albieh@me.com>
2023-10-16 18:48:51 +0100

    Teacher class added and accompanying tests. Also getters and setters for TeacherId was created and tested.

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,2 +1,17 @@
-public class Teacher {
+public class Teacher extends Person{
+
+    int teacherId;
+
+    public Teacher(String name, int age, String address, int teacherId){
+        super(name, age, address);
+        this.teacherId = teacherId;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
 }
--- src/test/java/TeacherTest.java
+++ src/test/java/TeacherTest.java
@@ -1,2 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class TeacherTest {
+
+    Teacher teacher;
+
+    @BeforeEach
+    public void setUp(){
+
+        teacher = new Teacher("John", 18, "10 Downing Street",123456789);
+
+    }
+
+    @Test
+    public void canGetName(){
+        assertThat(teacher.getName()).isEqualTo("John");
+    }
+    @Test
+    public void canGetAge(){
+        assertThat(teacher.getAge()).isEqualTo(18);
+    }
+    @Test
+    public void canGetAddress(){
+        assertThat(teacher.getAddress()).isEqualTo("10 Downing Street");
+    }
+
+    @Test
+    public void canGetStudentId(){
+        assertThat(teacher.getTeacherId()).isEqualTo(123456789);
+    }
+    @Test
+    public void canSetStudentId(){
+        teacher.setTeacherId(987654321);
+        assertThat(teacher.getTeacherId()).isEqualTo(987654321);
+    }
 }
