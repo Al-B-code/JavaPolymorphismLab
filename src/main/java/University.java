2ce0a000e977524545e55bf3a2e2adf2048e9b7c
Albert Hughes <albieh@me.com>
2023-10-17 17:45:23 +0100

    initial creation for the University class. Tests not created yet.

--- src/main/java/University.java
+++ src/main/java/University.java
@@ -5,10 +5,44 @@ import java.util.HashMap;
 
 public class University {
 
+    private String name;
     private ArrayList<Student> students;
     private ArrayList<Teacher> teachers;
     private ArrayList<Course> courses;
     private HashMap<Teacher, ArrayList<Course>> teacherCourseMap;
+
+
+    public void getName(String name){
+        this.name = name;
+    }
+
+    public String setName(String name){
+        this.name = name;
+    }
+
+    public void enroll(Student student){
+        students.add(student);
+    }
+
+    public void enroll(Teacher teacher){
+        teachers.add(teacher);
+    }
+
+    public ArrayList<Course> getCourses(){
+        return courses;
+    }
+
+    public void addCourse(Course course){
+        courses.add(course);
+    }
+    public ArrayList<Student> getStudents(){
+        return students;
+    }
+    public ArrayList<Teacher> getTeachers(){
+        return teachers;
+    }
+
+
 
 
 }
